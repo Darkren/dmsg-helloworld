1ea050164789820d61a4a23c5702584728bba415
Sir Darkrengarius <darkren92@gmail.com>
2019-07-26 06:32:38 +0300

    Add straightforward example

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Darkren/dmsg-helloworld
+
+go 1.12
+
+require github.com/skycoin/dmsg v0.0.0-20190719095515-52043626400c // indirect
---
+++ main.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	"github.com/skycoin/dmsg"
+	"github.com/skycoin/dmsg/cipher"
+	"github.com/skycoin/dmsg/disc"
+)
+
+func main() {
+	// generate keys for clients
+	rPK, rSK := cipher.GenerateKeyPair()
+	iPK, iSK := cipher.GenerateKeyPair()
+
+	// instantiate discovery
+	dc := disc.NewHTTP("https://messaging.discovery.skywire.skycoin.net")
+
+	// instantiate clients
+	responder := dmsg.NewClient(rPK, rSK, dc)
+	initiator := dmsg.NewClient(iPK, iSK, dc)
+
+	// connect to the DMSG server
+	if err := responder.InitiateServerConnections(context.Background(), 1); err != nil {
+		log.Fatalf("Error initiating server connections by responder: %v", err)
+	}
+
+	// connect to the DMSG server
+	if err := initiator.InitiateServerConnections(context.Background(), 1); err != nil {
+		log.Fatalf("Error initiating server connections by initiator: %v", err)
+	}
+
+	// dial responder via DMSG
+	iTp, err := initiator.Dial(context.Background(), rPK)
+	if err != nil {
+		log.Fatalf("Error dialing responder: %v", err)
+	}
+
+	// accept connection
+	rTp, err := responder.Accept(context.Background())
+	if err != nil {
+		log.Fatalf("Error accepting inititator: %v", err)
+	}
+
+	// initiator writes to his transport
+	_, err = iTp.Write([]byte("Hello there!"))
+	if err != nil {
+		log.Fatalf("Error writing to initiator's transport: %v", err)
+	}
+
+	// responder reads from his transport
+	recBuf := make([]byte, 12)
+	_, err = rTp.Read(recBuf)
+	if err != nil {
+		log.Fatalf("Error reading from responder's transport: %v", err)
+	}
+
+	log.Printf("Responder accepted: %s", string(recBuf))
+
+	// responder writes to his transport
+	_, err = rTp.Write([]byte("General Kenobi"))
+	if err != nil {
+		log.Fatalf("Error writing response: %v", err)
+	}
+
+	// initiator reads from his transport
+	iRecBuf := make([]byte, 14)
+	_, err = iTp.Read(iRecBuf)
+	if err != nil {
+		log.Fatalf("Error reading response: %v", err)
+	}
+
+	log.Printf("Initiator accepted: %s", string(iRecBuf))
+
+	// close transport
+	if err := iTp.Close(); err != nil {
+		log.Fatalf("Error closing initiator's transport: %v", err)
+	}
+
+	// close transport
+	if err := rTp.Close(); err != nil {
+		log.Fatalf("Error closing responder's transport: %v", err)
+	}
+
+	// close client
+	if err := initiator.Close(); err != nil {
+		log.Fatalf("Error closing initiator: %v", err)
+	}
+
+	// close client
+	if err := responder.Close(); err != nil {
+		log.Fatalf("Error closing responder: %v", err)
+	}
+}
